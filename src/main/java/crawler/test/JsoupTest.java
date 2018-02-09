424927c48061a64f612e7d6519eeff031142df25
zhaoqunDD <15962723902@163.com>
2018-02-09 14:26:52 +0800

    更新

--- src/main/java/crawler/test/JsoupTest.java
+++ src/main/java/crawler/test/JsoupTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/crawler/test/JsoupTest.java
 package crawler.test;
 
 import java.io.IOException;
@@ -17,67 +16,6 @@ import org.jsoup.select.Elements;
  * @author 赵群
  */
 public class JsoupTest {
-
-	public static void main(String[] args) {
-		/*
-		String url ="<html><head><title> 这里是字符串内容</title></head"+ ">"+"<body><p class='p1'> 这里是 jsoup 作用的相关演示</p></body></html>";
-		Document doc = (Document) Jsoup.parse(url);
-		
-		Elements links = ((Element) doc).select("p[class]");
-	       for(Element link:links){
-	        String linkclass = link.className();
-	            String linkText = link.text();
-	            System.out.println(linkText);
-	            System.out.println(linkclass);
-	        }
-	
-	*/
-		
-		try {
-			String url ="https://jiadian.jd.com/";
-			Document doc = (Document) Jsoup.connect(url).get();
-			
-			//得到html的所有东西
-			Element content = doc.getElementById("content");
-			/*
-			Elements links = content.getElementsByTag("a");
-			 Elements pngs = doc.select("img[src$=.png]");
-			 
-			 for (Element link : links) {
-			String linkHref = link.attr("href");
-			String linkText = link.text();
-			System.out.println(linkText);
-			}
-			 */
-			 System.out.println(doc);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}	
-	
-}
-=======
-package crawler.test;
-
-import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-/**
- * jsoup爬虫
- * jsoup 是一款Java 的HTML解析器，
- * 可直接解析某个URL地址、HTML文本内容。它提供了一套非常省力的API，
- * 可通过DOM，CSS以及类似于jQuery的操作方法来取出和操作数据。
- *2018年2月8日-下午5:29:08
- * @author 赵群
- */
-public class Test {
 
 	public static void main(String[] args) {
 		/*
@@ -120,4 +58,3 @@ public class Test {
 	}	
 	
 }
->>>>>>> 6885a1ef33ffa15e4e0efb92ad019fa6e0e19b48:src/main/java/crawler/test/Test.java
